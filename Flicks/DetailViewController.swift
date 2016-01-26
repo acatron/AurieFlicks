1d966a0c2ebb64981730dc46f83194c2d83bdbf1
Aurielle <Aurie@aurielles-macbook-pro.local>
2016-01-25 22:16:56 -0500

    Initial Commit

---
+++ Flicks/DetailViewController.swift
@@ -0,0 +1,65 @@
+//
+//  DetailViewController.swift
+//  MovieApp
+//
+//  Created by Aurielle on 1/25/16.
+//  Copyright © 2016 Aurielle. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
+    
+    var movie: NSDictionary!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        let title = movie["title"] as! String
+        let overview = movie["overview"]as! String
+        
+        titleLabel.text = title
+        overviewLabel.text = overview
+        
+        overviewLabel.sizeToFit()
+        
+        infoView.frame.size.height = 50 + overviewLabel.frame.size.height
+
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height + 50)
+        
+        let posterBaseUrl = "http://image.tmdb.org/t/p/w500";
+        if let posterPath = movie["poster_path"] as? String{
+            let posterUrl = NSURL(string: posterBaseUrl + posterPath);
+            posterImageView.setImageWithURL(posterUrl!);
+        }
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Flicks/MoviesViewController.swift
@@ -0,0 +1,147 @@
+//
+//  MoviesViewController.swift
+//  MovieApp
+//
+//  Created by Aurielle on 1/25/16.
+//  Copyright © 2016 Aurielle. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+import MBProgressHUD
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var movies: [NSDictionary]?
+    var filteredMovies: [NSDictionary]!
+    
+    var endpoint: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self;
+        tableView.delegate = self;
+        
+        let refreshControl = UIRefreshControl();
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged);
+        
+        tableView.insertSubview(refreshControl, atIndex: 0);
+        
+        searchBar.delegate = self;
+
+        getNowPlayingData();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let filteredMovies = filteredMovies {
+            return filteredMovies.count;
+        } else {
+            return 0;
+        }
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell;
+        
+        let movie = filteredMovies[indexPath.row];
+        
+        let title = movie["title"] as! String;
+        
+        let overview = movie["overview"] as! String;
+        
+        let posterBaseUrl = "http://image.tmdb.org/t/p/w500";
+        if let posterPath = movie["poster_path"] as? String{
+            let posterUrl = NSURL(string: posterBaseUrl + posterPath);
+            cell.posterView.setImageWithURL(posterUrl!);
+        }
+        
+        cell.titleLabel.text = title;
+        cell.overviewLabel.text = overview;
+        
+        return cell;
+    }
+    
+    func getNowPlayingData(){
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string:"https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            
+                            self.movies = responseDictionary["results"] as? [NSDictionary];
+                            self.filteredMovies = self.movies;
+                            
+                            MBProgressHUD.hideHUDForView(self.view, animated: true)
+                            
+                            self.tableView.reloadData();
+                    }
+                }
+        });
+        task.resume()
+    }
+    
+    func refreshControlAction(refreshControl: UIRefreshControl){
+        self.tableView.reloadData();
+        refreshControl.endRefreshing();
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredMovies = searchText.isEmpty ? movies : movies!.filter({(movieDictionary: NSDictionary) -> Bool in
+            return (movieDictionary["title"] as! String).rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+        })
+        
+        self.tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPathForCell(cell)
+        let movie = filteredMovies![indexPath!.row]
+        
+        let detailViewController = segue.destinationViewController as! DetailViewController
+        detailViewController.movie = movie
+        
+    }
+
+    
+
+}
